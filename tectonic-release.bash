7749da259ae69b1ef5b982513110ad7514351464
Kye Shi <shi.kye@gmail.com>
2021-09-12 16:46:38 -0700

    delete existing release first

--- tectonic-release.bash
+++ tectonic-release.bash
@@ -8,14 +8,30 @@ readarray -t patterns <<< "$2"
 for pattern in "${patterns[@]}"; do
   # shellcheck disable=SC2206
   IFS= paths=($pattern)
-
   for path in "${paths[@]}"; do
+
+    echo "::group::Compiling ${path@Q}"
     tectonic -X -c 'minimal' compile "$path"
+    echo '::endgroup::'
+
   done
 done
 
-curl -fsS \
-  -H "Authorization: Bearer $1" \
-  -H 'Accept: application/vnd.github.v3+json' \
-  -d '{"tag_name": "latest"}' \
+echo '::group::Creating release'
+
+curl=(
+  curl -fsS
+  -H "Authorization: Bearer $1"
+  -H 'Accept: application/vnd.github.v3+json'
+)
+
+"${curl[@]}" -X 'DELETE' \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/latest"
+
+"${curl[@]}" -d '{"tag_name": "latest"}' \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/releases"
+
+"${curl[@]}" \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/latest/assets"
+
+echo '::endgroup::'
