be3f1de1ca0c96fbaa6a907c89670ea5e3ffb03e
Kye Shi <shi.kye@gmail.com>
2021-09-12 16:22:55 -0700

    compile, try curl api

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 # vi: ft=dockerfile
 FROM alpine:latest
-RUN ["apk", "add", "bash", "tectonic", "findutils"]
+RUN ["apk", "add", "bash", "tectonic", "curl"]
 COPY ["tectonic-release.bash", "/bin/tectonic-release"]
 RUN ["chmod", "+x", "/bin/tectonic-release"]
 ENTRYPOINT ["/bin/tectonic-release"]
-CMD [""]
--- tectonic-release.bash
+++ tectonic-release.bash
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-echo hi
+shopt -s nullglob dotglob extglob globstar
 
-echo "NUMBER OF ARGS: $#"
+readarray -t patterns <<< "$1"
+
+for pattern in "${patterns[@]}"; do
+  # shellcheck disable=SC2206
+  IFS= paths=($pattern)
+
+  for path in "${paths[@]}"; do
+    tectonic -X compile "$path"
+  done
+done
+
+curl "https://api.github.com/repos/$GITHUB_REPOSITORY/releases"
