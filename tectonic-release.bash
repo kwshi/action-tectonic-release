42fd092983434b17ceb0babce5efed6c9b02d0c2
Kye Shi <shi.kye@gmail.com>
2021-09-12 17:38:59 -0700

    make names match?

--- tectonic-release.bash
+++ tectonic-release.bash
@@ -42,7 +42,7 @@ curl -fsS \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H 'Accept: application/vnd.github.v3+json' \
   --data-binary '@lgcs105.cls' \
-  "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$id/assets?name=lgcs105cls"
+  "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$id/assets?name=lgcs105.cls"
 #api "releases/$id/assets?name=lgcs105cls" --data-binary '@lgcs105.cls'
 #api 'releases/assets"
 
