d56cba5f8339fd90bbaadd3e52917f8189886aaf
Kye Shi <shi.kye@gmail.com>
2021-09-12 18:26:03 -0700

    upload pdfs

--- tectonic-release.bash
+++ tectonic-release.bash
@@ -3,48 +3,50 @@ set -euo pipefail
 shopt -s nullglob dotglob extglob globstar
 
 declare GITHUB_TOKEN="$1" PATTERNS="$2"
-
-readarray -t patterns <<< "$PATTERNS"
-for pattern in "${patterns[@]}"; do
-  # shellcheck disable=SC2206
-  IFS= paths=($pattern)
-  for path in "${paths[@]}"; do
-
-    echo "::group::Compiling ${path@Q}"
-    tectonic -X -c 'minimal' compile "$path"
-    echo '::endgroup::'
-
-  done
-done
-
-echo '::group::Creating release'
-
-CURL=(
+declare -a CURL_API=(
   curl -fsS
   -H "Authorization: Bearer $GITHUB_TOKEN"
   -H 'Accept: application/vnd.github.v3+json'
 )
 
-echo 'checking for existing release'
-if read -r id < <("${CURL[@]}" \
+declare -a patterns paths=()
+declare path pdf
+
+readarray -t patterns <<< "$PATTERNS"
+for pattern in "${patterns[@]}"; do
+  # shellcheck disable=SC2206
+  IFS= paths+=($pattern)
+done
+
+for path in "${paths[@]}"; do
+  echo "::group::Compiling ${path@Q}"
+  tectonic -X -c 'minimal' compile "$path"
+  echo '::endgroup::'
+done
+
+echo '::group::Creating release'
+if read -r id < <("${CURL_API[@]}" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/latest" \
   | jq -rc '.id'
   ); then
   echo 'existing release, deleting'
-  "${CURL[@]}" -X 'DELETE' \
+  "${CURL_API[@]}" -X 'DELETE' \
     "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/$id"
 fi
-
-echo 'creating new release'
-read -r id < <("${CURL[@]}" -d '{"tag_name": "latest"}' \
+read -r id < <("${CURL_API[@]}" -d '{"tag_name": "latest"}' \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/releases" \
   | jq -rc '.id'
 )
+echo '::endgroup::'
 
-echo 'uploading release assets'
-"${CURL[@]}" \
-  -H 'Content-Type: text/plain' \
-  --data-binary '@lgcs105.cls' \
-  "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$id/assets?name=a/lgcs105.cls&label=hello/test"
+echo '::group::Uploading release assets'
+for path in "${paths[@]}"; do
+  pdf="${path%.*}.pdf"
+  echo "$path"
+  "${CURL_API[@]}" \
+    -H 'Content-Type: application/pdf' \
+    --data-binary "@$pdf" \
+    "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$id/assets?name=$pdf&label=$pdf"
 
+done
 echo '::endgroup::'
