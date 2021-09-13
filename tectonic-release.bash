f44c1ae6e979b97fd79f9e2e122e6195b2a3ac53
Kye Shi <shi.kye@gmail.com>
2021-09-12 17:25:49 -0700

    try using upload url

--- tectonic-release.bash
+++ tectonic-release.bash
@@ -38,7 +38,12 @@ echo 'creating new release'
 read -r id < <(api 'releases' -d '{"tag_name": "latest"}' | jq -rc '.id')
 
 echo 'uploading release assets'
-api "releases/$id/assets?name=lgcs105cls" --data-binary '@lgcs105.cls'
+curl -fsS \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H 'Accept: application/vnd.github.v3+json' \
+  --data-binary '@lgcs105.cls'
+  "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$id/assets?name=lgcs105cls"
+#api "releases/$id/assets?name=lgcs105cls" --data-binary '@lgcs105.cls'
 #api 'releases/assets"
 
 echo '::endgroup::'
