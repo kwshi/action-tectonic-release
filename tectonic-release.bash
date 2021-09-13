258cd1ee0523f664faa1ade00a74c75e78425e0c
Kye Shi <shi.kye@gmail.com>
2021-09-12 18:06:30 -0700

    what if I use raw slashes

--- tectonic-release.bash
+++ tectonic-release.bash
@@ -45,6 +45,6 @@ echo 'uploading release assets'
 "${CURL[@]}" \
   -H 'Content-Type: text/plain' \
   --data-binary '@lgcs105.cls' \
-  "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$id/assets?name=lgcs105.cls&label=hello%2Ftest"
+  "https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$id/assets?name=a/lgcs105.cls&label=hello/test"
 
 echo '::endgroup::'
